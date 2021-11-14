fe77530bf79eb19e8f4ff1a17e81288a43d42447
Gabriel Tarifa <gabrieltarifa@gmail.com>
2021-11-14 17:37:25 -0300

    Se agrega la llamada a la api de openweathermap

--- navigation/MyListStack.js
+++ navigation/MyListStack.js
@@ -1,8 +1,9 @@
 import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
-import {createStackNavigator} from '@react-navigation/stack';
+import { StyleSheet, Text, View } from 'react-native';
+import { createStackNavigator } from '@react-navigation/stack';
 import MyListScreen from './../screen/myList/MyListScreen';
 import SearchCityScreen from '../screen/SearchCityScreen';
+import CityScreen from '../screen/myList/CityScreen';
 
 const Stack = createStackNavigator();
 
@@ -10,7 +11,7 @@ const MyListStack = () => {
   return (
     <Stack.Navigator>
       <Stack.Screen name="MyListScreen" component={MyListScreen} />
-      <Stack.Screen name="CityScreen" component={SearchCityScreen} />
+      <Stack.Screen name="CityScreen" component={CityScreen} />
     </Stack.Navigator>
   );
 };
--- screen/myList/MyListScreen.js
+++ screen/myList/MyListScreen.js
@@ -1,18 +1,72 @@
 import React from 'react';
-import {Button, StyleSheet, Text, View} from 'react-native';
+import { Button, StyleSheet, Text, View, TouchableOpacity, FlatList, Image } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
+
+// Se debería obtener de la base de datos
+const CITIES = [
+  {
+    id: "1",
+    title: "Buenos Aires",
+    latitude: -37.97,
+    longitude: -58.37,
+  },
+  {
+    id: "2",
+    title: "La Rioja",
+    latitude: -29.41,
+    longitude: -66.85,
+  },
+  {
+    id: "3",
+    title: "Mendoza",
+    latitude: -32.88,
+    longitude: -68.83,
+  },
+  {
+    id: "4",
+    title: "Córdoba",
+    latitude: -31.41,
+    longitude: -64.18,
+  },
+];
+
+const CityItem = ({ item, onPress }) => (
+  <TouchableOpacity onPress={onPress}>
+    <Text style={styles.cityItem}>{item.title}</Text>
+  </TouchableOpacity>
+);
+
 
 const MyListScreen = () => {
 
   const navigation = useNavigation();
 
+  const renderItem = ({ item }) => {
+    const backgroundColor = '#5a78f2';
+    const color = 'white';
+
+    return (
+      <CityItem
+        item={item}
+        onPress={() => {
+          navigation.navigate('CityScreen', {
+            city: item.title,
+            latitude: item.latitude,
+            longitude: item.longitude,
+          });
+        }}
+        backgroundColor='#5a78f2'
+      />
+    );
+  };
+
   return (
-    <View>
-      <Text>My List</Text>
-      <Button
-        title='City'
-        color='#080'
-        onPress={() => navigation.navigate('CityScreen')}
+    <View style={styles.container}>
+      <Text style={styles.title}>Mi lista de ciudades</Text>
+      <FlatList
+        data={CITIES}
+        renderItem={renderItem}
+        keyExtractor={(item) => item.id}
       />
     </View>
   );
@@ -20,4 +74,25 @@ const MyListScreen = () => {
 
 export default MyListScreen;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    backgroundColor: '#7AD',
+  },
+  cityItem: {
+    fontSize: 20,
+    color: '#fff',
+    padding: 10,
+    borderBottomColor: '#DDE',
+    borderBottomWidth: 1,
+  },
+  title: {
+    fontSize: 22,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    padding: 10,
+    borderBottomColor: '#DDE',
+    borderBottomWidth: 2,
+  }
+});
